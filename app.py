a6695125ee92b35e3424ae315c32de09fa7e9fa3
Matheus Marques <matheusmarquesportela@gmail.com>
2023-08-18 15:24:30 -0300

    conclusão mobile first

---
+++ app.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+nome = "Matheus" #string
+idade = 20 #int
+altura = 1.86 #float
+is_male = True #bool
+gosto_de = "F1"
+
+print("Meu nome é " + nome + ".")
+print(f"Eu gosto de {gosto_de}.")
+print(f"Eu tenho {idade} anos.")
+
+idade = "34"
+
+print(f"{nome} gosta de {gosto_de} e tem {idade} anos.")
